893a8dea7a588eac7681c40a0123d4f4b17a9aff
Sultan Amiour <sultan.amiour@epita.fr>
2025-12-08 03:37:27 +0100

    feat: all redirection uses Link component for performance. Change .html routes to directory routes.

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Card from './Card'
+import { Link } from 'react-router-dom'
 
 const HomeCard = () => {
     return (
@@ -11,12 +12,12 @@ const HomeCard = () => {
                         <p className="mt-2 mb-4">
                             Browse our React jobs and start your career today
                         </p>
-                        <a
-                            href="/jobs.html"
+                        <Link
+                            to="/jobs"
                             className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
                         >
                             Browse Jobs
-                        </a>
+                        </Link>
                     </Card>
                     {/*<div className="bg-indigo-100 p-6 rounded-lg shadow-md">*/}
                     {/*    */}
@@ -26,12 +27,12 @@ const HomeCard = () => {
                         <p className="mt-2 mb-4">
                             List your job to find the perfect developer for the role
                         </p>
-                        <a
-                            href="/add-job.html"
+                        <Link
+                            to="/add-job"
                             className="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
                         >
                             Add Job
-                        </a>
+                        </Link>
                     </Card>
                 </div>
             </div>
--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { FaMapMarker } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const Job = ({ job }) => {
     // can be used for burger menu, sidebar menu, or modal toggle, etc
@@ -36,12 +37,12 @@ const Job = ({ job }) => {
                         <FaMapMarker className="inline text-lg mb-1 mr-1"/>
                         {job.location}
                     </div>
-                    <a
-                        href="job.html"
+                    <Link
+                        to="/jobs"
                         className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                     >
                         View More
-                    </a>
+                    </Link>
                 </div>
             </div>
         </div>
